9f43bfd88bb2f2c407972fa4ad907fbc8c1ab2ce
Ibrameias <ibramdarwish@gmail.com>
2022-01-30 13:03:06 -0500

    create landing page in page folder and import the components to use them in this page

---
+++ causerages/src/page/index.js
@@ -0,0 +1,8 @@
+// import the components / assets to use them in the page 
+import Header from "../Header"
+import Footer from "../Footer";
+import Button from "../Button";
+import logo from "../../assets/logo.svg";
+import logoWhite from "../../assets/logo_white.svg";
+import bg from "../../assets/bg.jpg";
+import './landingPage.css'
